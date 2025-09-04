96f87968a637154b6a6fe3a1fdf4d1f052fbcc1c
Devpot-abhi <abhishekgurjar5020@gmail.com>
2025-09-04 11:20:25 +0530

    03-09-2025

---
+++ html/newnew.js
@@ -0,0 +1,25 @@
+
+
+var n=15;
+// var str="myname name is abhishek "
+
+var num=45791
+
+// for(var i=5;i<=n;i++){
+
+//      if(i%2==0){
+//           console.log(i-1)
+//      }
+//      else{
+//           console.log(i%2==1)
+//      }
+//      if(i%3==0 && i%5==0){
+//           console.log("5"+2)
+//      }
+//      else if(i%3==0){
+//           console.log(1+"1")
+//      }
+//      else{
+//           console.log("i"+0)
+//      }
+// }
---
+++ images/new.py
@@ -0,0 +1,51 @@
+# a=10;
+# b=4.5;
+# name1="abhi"
+# name2="jay"
+# c='A';
+
+
+# first_name="abhishek"
+# firstName="abhishek"
+
+# age=20;
+# Age=30;
+
+# # if=34;
+# # import keyword
+# # print(keyword.kwlist)
+
+# print(a,b)
+
+
+# print(5**3) #5*5*5
+
+# print(15//2)
+
+# print(5%3)
+# print(6%2)
+# print(7%3)
+# # print(7%0)
+# print(0%3)
+
+
+# print(5%7)
+# print(7%5)
+
+
+
+a=4;
+b=5;
+
+# print(a+b)
+# print(a%3)
+print(a>=2 or b<=5)
+# print(b%a)
+# a-=2
+# print(b%4)
+print(a%3!=1 and b%2==1)
+print(a%b)
+a+=b
+print(b%6)
+b+=3
+print(a%b)
